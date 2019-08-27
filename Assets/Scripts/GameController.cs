3a422470f1efadf2f2595ae9333c648c2e580b80
DrHurtful <12wilki12@gmail.com>
2019-08-27 22:38:26 +0300

    GameController.
    Added Score manager, so far: space to add score H to remove. if score less then 0 u lose.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    public Text scoretext;
+
+    private bool GameEnded = false;
+
+
+    private int BaseScore = 0;
+    public int score;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = BaseScore;
+        UpdateScore();
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown (KeyCode.Space) && !GameEnded)
+        {
+            score += 100;
+            UpdateScore();
+        }
+        if (Input.GetKeyDown(KeyCode.H) && !GameEnded)
+        {
+            score -= 50;
+            UpdateScore();
+        }
+
+
+        if (score < 0 && !GameEnded)
+        {
+            Debug.Log("GameEnded");
+            GameEnded = true;
+        }
+        
+    }
+
+    private void UpdateScore()
+    {
+        scoretext.text = score.ToString(); if (score <= 0)
+        
+
+        {
+            scoretext.text = "0"; 
+        }
+    }
+}
